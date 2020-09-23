15a9861cd3df1d82e1c41f1af20dac8369ed1504
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-09-22 18:58:57 -0700

    moving files....

--- move_data.py
+++ move_data.py
@@ -5,10 +5,10 @@ source_paths = "/root/my_workspace/data/modified_paths/"
 source_costmaps = "/root/my_workspace/data/modified_costmaps/"
 
 destination_train_paths = "/root/my_workspace/data/main_train/train/paths/"
-destination_train_costmaps = "/root/my_workspace/data/main_train/train/paths/"
+destination_train_costmaps = "/root/my_workspace/data/main_train/train/costmaps/"
 
 destination_test_paths = "/root/my_workspace/data/main_train/test/paths/"
-destination_test_costmaps = "/root/my_workspace/data/main_train/test/paths/"
+destination_test_costmaps = "/root/my_workspace/data/main_train/test/costmaps/"
 
 files_paths = os.listdir(source_paths)
 files_costmaps = os.listdir(source_costmaps)
@@ -16,10 +16,13 @@ files_costmaps = os.listdir(source_costmaps)
 # train_paths
 count = 1
 for file in files_paths:
+	print("inside for loop")
 	if(count>=1 and count<=20000):
+		print("inside first if condition")
 		shutil.copy(file, destination_train_paths)
 		count+=1
 	elif(count>20000 and count<=25000):
+		print("inside second if condition")
 		shutil.copy(file, destination_test_paths)
 		count+=1
 	else:

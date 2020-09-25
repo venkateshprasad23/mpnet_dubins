9b1021cb8d28933f4eceb61a00bbdcd166b12190
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-09-25 13:39:17 -0700

    plot3

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ datafile = open('/root/my_workspace/data/trained_models/progress.csv', 'r')
 myreader = csv.reader(datafile)
 
 array = []
-array = np.array(array)
+# array = np.array(array)
 
 for row in myreader:
 	array.append(row[1])

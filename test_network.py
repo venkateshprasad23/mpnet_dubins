f796427001966d1db996881b263fc8019a88949a
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-06 13:03:17 -0700

    testing network16

--- test_network.py
+++ test_network.py
@@ -91,7 +91,7 @@ if __name__=="__main__":
     with torch.no_grad():
         # test loss
         network_output = mpnet_base.mpNet(testInput, testObs).data.cpu()
-        print(network_output[:,:2].shape)
+        print(network_output[:1,:].shape)
         # network_output[,:2] = unnormalize(network_output.squeeze(),worldSize)
         # test_loss_i = mpnet_base.mpNet.loss(
         #     network_output,

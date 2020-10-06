f0987f347db597cdc4ef59488b6f7dcd5db89aab
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-05 17:34:09 -0700

    testing network8

--- test_network.py
+++ test_network.py
@@ -79,9 +79,10 @@ if __name__=="__main__":
         testObs, testInput, testTarget)
 
     if torch.cuda.is_available():
-            mpnet_base.mpNet.cuda()
-            mpnet_base.mpNet.mlp.cuda()
-            mpnet_base.mpNet.encoder.cuda()
+    	print("CUDA is available!")
+        mpnet_base.mpNet.cuda()
+        mpnet_base.mpNet.mlp.cuda()
+        mpnet_base.mpNet.encoder.cuda()
 
     with torch.no_grad():
         # test loss

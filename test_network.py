9142d4d8fdcb2816f8fbacdde6ea2a3849e04643
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-06 17:20:01 -0700

    testing network23

--- test_network.py
+++ test_network.py
@@ -91,7 +91,7 @@ if __name__=="__main__":
     # Do a test sampling using the sample code
     s = 30
     # observation = env.reset()
-    costmap = np.load('/root/my_workspace/data/modified_costmaps/' + str(s) + '.npy')
+    costmap = np.load('/root/my_workspace/data/modified_costmaps/' + str(s) + '.npy')[0]
     traj = np.load('/root/my_workspace/data/ref_paths/' + str(s) + '.npy',allow_pickle=True)
     localtraj = np.copy(traj)
 

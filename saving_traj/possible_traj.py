f4c872c8b5bb12f2bae5edd1d4591144835bafed
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-09 10:00:50 -0700

    dta6

--- saving_traj/possible_traj.py
+++ saving_traj/possible_traj.py
@@ -23,6 +23,7 @@ def get_points(point):
 
 def check_dist(x,y):
     print(math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2))
+    print(math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2)<=1)
     return math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2)<=1
 
 def PossibleComb(points):

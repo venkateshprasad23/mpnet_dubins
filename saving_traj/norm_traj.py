da5ae83e17b6a8e3dc43fc2bec4ee53bacfadccb
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-08 18:13:28 -0700

    debugging data25

--- saving_traj/norm_traj.py
+++ saving_traj/norm_traj.py
@@ -98,17 +98,18 @@ if __name__ == "__main__":
             # seeds.append(s)
             path_array = []
             traj = np.load(osp.join(trajFolder,entry),allow_pickle=True)
+            print(traj.shape)
             traj = np.reshape(traj,(traj.shape[0],1))
             # View trajectories from the perspective of the local costmap
             localtraj = np.copy(traj)
             # print(PossibleComb(localtraj))
             # PossibleComb(localtraj)
-            start = localtraj[0]
-            start_x, start_y, start_z = get_points(start)
+            start_x, start_y, start_z = localtraj[0]
+            # start_x, start_y, start_z = get_points(start)
             # path_array.append(start_x, start_y, start_z)
             count = count+1
             for points in localtraj:
-                x,y,z = get_points(points)
+                x,y,z = points
                 x = x - start_x
                 y = y - start_y
                 z = z - start_z

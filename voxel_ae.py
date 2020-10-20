662ecd4ea8835b417f96ac14d1c16a554f250e36
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-20 14:09:56 -0700

    head input change

--- voxel_ae.py
+++ voxel_ae.py
@@ -55,7 +55,7 @@ class Encoder(nn.Module):
         for n in x.size()[1:]:
             first_fc_in_features *= n
         self.head = nn.Sequential(
-            nn.Linear(first_fc_in_features + state_size + 1, 128),
+            nn.Linear(first_fc_in_features + state_size, 128),
             nn.PReLU(),
             nn.Dropout(),
             nn.Linear(128, output_size),

51ba50e45084c82eca2698c7ecc5bc471e9662c2
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-20 09:56:57 -0700

    try training19

--- mpnet.py
+++ mpnet.py
@@ -114,11 +114,13 @@ class MPnetBase():
         Formats the input data that needed to be fed into the network
         """
         if isinstance(inputs, np.ndarray):
-            bi = torch.HalfTensor(inputs)
+            bi = torch.Tensor(inputs)
+            bi = bi.half()
         else:
             bi = inputs.float()
         if isinstance(obs, np.ndarray):
-            bobs = torch.HalfTensor(obs)
+            bobs = torch.Tensor(obs)
+            bobs = bobs.half()
         else:
             bobs = obs.float()
 
@@ -136,7 +138,8 @@ class MPnetBase():
         bobs, bi = self.format_input(obs, inputs)
         # Format targets
         if isinstance(targets, np.ndarray):
-            bt = torch.HalfTensor(targets)
+            bt = torch.Tensor(targets)
+            bt = bt.half()
         else:
             bt = targets.float()
         bt = self.normalize(bt, self.worldSize)

be5c371d97f440617543821075a3f6ddffb9e333
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-20 09:29:55 -0700

    try training15

--- main_train.py
+++ main_train.py
@@ -21,8 +21,8 @@ def train(args):
         'encoderOutputDim': 128,
         # 'worldSize': [27, 27, np.pi],
         'worldSize' : [2, 2, 2],
-        'AE': voxelNet.half(),
-        'MLP': MLP.half(),
+        'AE': voxelNet,
+        'MLP': MLP,
         'modelPath': args.file,
     }
     

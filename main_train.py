855fd6efd4a0f1c03615a98134058f50744ec7a9
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-24 09:38:01 -0700

    half tensor2

--- main_train.py
+++ main_train.py
@@ -10,7 +10,7 @@ import model as model
 from misc import normalize, unnormalize
 from train import MPnetTrain
 
-torch.set_default_tensor_type(torch.cuda.HalfTensor)
+torch.set_default_tensor_type(torch.HalfTensor)
 # torch.multiprocessing.set_start_method('spawn',force=True)
 
 

7f3b2c225fea2d88b386079a5aad83e90581eaaf
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-26 10:12:40 -0700

    debug memory

--- main_train.py
+++ main_train.py
@@ -12,6 +12,16 @@ from train import MPnetTrain
 
 torch.cuda.empty_cache()
 
+def debug_memory():
+    import collections, gc, resource
+    print('maxrss = {}'.format(
+        resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
+    tensors = collections.Counter((str(o.device), o.dtype, tuple(o.shape))
+                                  for o in gc.get_objects()
+                                  if torch.is_tensor(o))
+    for line in sorted(tensors.items()):
+        print('{}\t{}'.format(*line))
+
 # torch.set_default_tensor_type(torch.HalfTensor)
 # torch.multiprocessing.set_start_method('spawn',force=True)
 

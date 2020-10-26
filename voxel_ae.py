b6a178ba8227f525eac2b5d7c597fd1c68397fdf
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-26 10:45:24 -0700

    randomly changed first fc in features again

--- voxel_ae.py
+++ voxel_ae.py
@@ -49,13 +49,14 @@ class Encoder(nn.Module):
         )
         # self.encoder.apply(weights_init)
         # For accepting different input shapes
-        x = self.encoder(torch.autograd.Variable(torch.rand(input_size)))
-        # x = self.encoder(torch.autograd.Variable(torch.rand([1] + input_size)))
+        # x = self.encoder(torch.autograd.Variable(torch.rand(input_size)))
+        x = self.encoder(torch.autograd.Variable(torch.rand([1] + input_size)))
         first_fc_in_features = 1
         for n in x.size()[1:]:
             first_fc_in_features *= n
         self.head = nn.Sequential(
-            nn.Linear(first_fc_in_features + state_size, 128),
+            nn.Linear(first_fc_in_features, 128),
+            # nn.Linear(first_fc_in_features + state_size, 128),
             nn.PReLU(),
             nn.Dropout(),
             # nn.Linear(128, output_size),

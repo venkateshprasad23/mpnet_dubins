5c59d5601b8a28a32b9f2583832a6b5733f5af6a
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-27 10:22:39 -0700

    testing dataloader

--- test_data_loader.py
+++ test_data_loader.py
@@ -5,19 +5,19 @@ from my_data_loader import ThreedDataset, ThreedIterDataset
 if __name__ == "__main__":
     trainDataFileName = '/root/my_workspace/data/main_train/train/'
     train_ds = ThreedIterDataset(trainDataFileName)
-    train_dl = DataLoader(train_ds, num_workers=5, batch_size=16, drop_last=True)
-    print("hello1")
-    count = 0
+    # train_dl = DataLoader(train_ds, num_workers=5, batch_size=16, drop_last=True)
+    # print("hello1")
+    # count = 0
 
-    # print(train_ds)
-    # print(train_dl[5])
-    for batch in train_dl:
-        print("hello2")
-        # print(x)
-        bobs, bi, bt = batch
-        print(bobs,bi,bt)
-    	# count+=1
-    	# print(count)
+    # # print(train_ds)
+    # # print(train_dl[5])
+    # for batch in train_dl:
+    #     print("hello2")
+    #     # print(x)
+    #     bobs, bi, bt = batch
+    #     print(bobs,bi,bt)
+    # 	# count+=1
+    # 	# print(count)
 
-    	# print("\n")
-    	# print(x)
+    # 	# print("\n")
+    # 	# print(x)

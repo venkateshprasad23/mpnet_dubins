4457853427b6a17ff38d4a67004049f425089c43
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-27 11:03:27 -0700

    testing dataloader

--- test_data_loader.py
+++ test_data_loader.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     trainDataFileName = '/root/my_workspace/data/main_train/train/'
     train_ds = ThreedIterDataset(trainDataFileName)
     train_dl = DataLoader(train_ds, batch_size=16, drop_last=True)
-    # print("hello1")
+    print("hello1")
     # count = 0
 
     # # print(train_ds)

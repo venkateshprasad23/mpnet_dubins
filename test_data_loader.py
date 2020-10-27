d28cfbe5320550e1b2776189631e17d8649f7571
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-26 18:49:28 -0700

    remove shuffle

--- test_data_loader.py
+++ test_data_loader.py
@@ -5,7 +5,7 @@ from my_data_loader import ThreedDataset, ThreedIterDataset
 if __name__ == "__main__":
     trainDataFileName = '/root/my_workspace/data/main_train/train/'
     train_ds = ThreedIterDataset(trainDataFileName)
-    train_dl = DataLoader(train_ds, shuffle=True, num_workers=5, batch_size=16, drop_last=True)
+    train_dl = DataLoader(train_ds, num_workers=5, batch_size=16, drop_last=True)
 
     count = 0
     # print(train_dl[5])

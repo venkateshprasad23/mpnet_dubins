a608c3d5a7da071e563b7d1752f6d6ddc7b5c6cc
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-26 18:55:27 -0700

    remove shuffle

--- test_data_loader.py
+++ test_data_loader.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     print("hello1")
     count = 0
 
-    print(x in train_dl)
+    print(train_ds)
     # print(train_dl[5])
     for x in train_dl:
         print("hello2")

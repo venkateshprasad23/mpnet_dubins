2717bf215233d34a96f75b3730db7588b937a90c
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2020-10-28 12:34:17 -0700

    put training

--- main_train.py
+++ main_train.py
@@ -51,7 +51,7 @@ def train(args):
     # trainNetwork.set_model_train_epoch(999)
     debug_memory()
     trainNetwork.train(numEnvsTrain=150000,
-                       numEnvsTest=10000,
+                       numEnvsTest=1000,
                        numPaths=1,
                        trainDataPath='/root/my_workspace/data/main_train/train/',
                        testDataPath='/root/my_workspace/data/main_train/test/')

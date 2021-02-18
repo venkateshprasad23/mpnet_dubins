e9ea87558856606dc19088e9e8956c1b1f7905d9
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2021-02-17 20:15:48 -0800

    hmm6

--- linjun.py
+++ linjun.py
@@ -5,6 +5,6 @@ import open3d as o3d
 
 my_traj = np.load('my_traj.npy')
 original_traj = np.load('/root/my_workspace/data/main_train/train/paths/2.npy')
-
-print(my_traj)
-print(original_traj)
+mapp = np.load('/root/my_workspace/mpnet_dubins/saving_traj/map_v2.npy')
+# print(my_traj)
+print(mapp.shape)

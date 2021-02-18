f340a33d31446acdd33f2aa8a79b9dffc171f3fa
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2021-02-17 20:17:50 -0800

    hmm6

--- linjun.py
+++ linjun.py
@@ -7,4 +7,9 @@ my_traj = np.load('my_traj.npy')
 original_traj = np.load('/root/my_workspace/data/main_train/train/paths/2.npy')
 mapp = np.load('/root/my_workspace/mpnet_dubins/saving_traj/map_v2.npy')
 # print(my_traj)
-print(mapp.shape)
+# print(mapp.shape)
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(mapp)
+
+o3d.visualization.draw_geometries([pcd])
+

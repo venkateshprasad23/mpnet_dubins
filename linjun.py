6327d7f0f18424b52e66c5f9c08dae5f9a666714
venkateshprasad23 <venkateshprasad23061997@gmail.com>
2021-02-17 23:40:30 -0800

    hmm6

--- linjun.py
+++ linjun.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import open3d as o3d
+from mpl_toolkits import mplot3d
 
 
 my_traj = np.load('my_traj.npy')
@@ -8,8 +9,17 @@ original_traj = np.load('/root/my_workspace/data/main_train/train/paths/2.npy')
 mapp = np.load('/root/my_workspace/mpnet_dubins/saving_traj/map_v2.npy')
 # print(my_traj)
 # print(mapp.shape)
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(mapp)
+# pcd = o3d.geometry.PointCloud()
+# pcd.points = o3d.utility.Vector3dVector(mapp)
 
-o3d.visualization.draw_geometries([pcd])
+# o3d.visualization.draw_geometries([pcd])
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+x = my_traj[:,0]
+y = my_traj[:,1]
+z = my_traj[:,2] 
+ax.scatter3D(x,y,z)
+plt.savefig('my_traj.png')
 
